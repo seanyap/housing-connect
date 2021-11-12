b3085898588a9398781a6c52d4beadb1b6ca49e5
Sean Yap <61310256+seanyap@users.noreply.github.com>
2021-11-12 13:29:04 -0500

    added landing page and used it in app.js (#6)
    * added navbar component
    
    * added search bar component
    
    * system modified package-json
    
    * change searchbar component name
    
    * added landing page and used it in app.js

---
+++ client/src/pages/LandingPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import SearchBar from "../components/SearchBar";
+
+function LandingPage() {
+  return (
+    <div id="center">
+      <h2>Find your next home</h2>
+      <SearchBar />
+    </div>
+  );
+}
+
+export default LandingPage;
