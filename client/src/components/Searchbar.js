1f0fc2c7bcee46729f2f33d6992e9288af972b0c
Sean Yap <syap0317@gmail.com>
2021-11-12 12:45:31 -0500

    added search bar component

---
+++ client/src/components/Searchbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Searchbar() {
+  return (
+    <div id="search">
+      <input type="text" placeholder="Enter an address, city or ZIP code" />
+      <button>Search</button>
+    </div>
+  );
+}
+
+export default Searchbar;
