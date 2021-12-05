47aef2efa4aae33b3c582ac87be12ff528c7130c
malyesh <74512928+malyesh@users.noreply.github.com>
2021-12-05 12:43:50 -0500

    edited styling for the login page

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -6,79 +6,64 @@ class LoginPage extends React.Component {
   render() {
     return (
       <div>
-        {/* <NavBar /> */}
-        <div class='page'>
-          <div class='login'>
-            <h2>Sign In</h2>
-            <label for='uname'>Email Address</label>
-            <input
-              type='text'
-              placeholder='Enter Email'
-              name='uname'
-              required
-            />
-            <br />
-            <label for='passw'>Password</label>
-            <input
-              type='password'
-              placeholder='Enter Password'
-              name='psw'
-              required
-            />
-            <br />
-            <button class='loginbtn' type='submit'>
-              Log In
-            </button>
-          </div>
-          <div class='signup'>
-            <h2>Sign Up</h2>
-            <div class='checkboxes'>
-              <div class='check'>
-                <input type='checkbox' id='tenant-check' />
-                <label for='tenant-check'>Tenant</label>
-              </div>
-
-              <div class='check'>
-                <input type='checkbox' id='owner-check' />
-                <label for='owner-check'>Homeowner</label>
-              </div>
+        <div class='body'>
+          <div class='login-body'>
+            <div id='login'>
+              <h2>Sign In</h2>
+              <form class='login'>
+                <label for='username'>Email Address</label>
+                <input type='text' placeholder='Enter Email' name='username' />
+                <br />
+                <label for='password'>Password</label>
+                <input
+                  type='password'
+                  placeholder='Enter Password'
+                  name='password'
+                />
+                <br />
+                <p>Don't have an account? Sign up for one for free!</p>
+                <input type='submit' value='Sign In' />
+              </form>
             </div>
-
-            <label for='fname'>Full Name</label>
-            <input
-              type='text'
-              placeholder='Enter Full Name'
-              name='fname'
-              required
-            />
-            <br />
-            <label for='uname'>Email Address</label>
-            <input
-              type='text'
-              placeholder='Enter Email'
-              name='uname'
-              required
-            />
-            <br />
-            <label for='passw'>Password</label>
-            <input
-              type='password'
-              placeholder='Enter Password'
-              name='psw'
-              required
-            />
-            <br />
-            <label for='confirm'>Confirm Password</label>
-            <input
-              type='password'
-              placeholder='Confirm Password'
-              name='confirm'
-              required
-            />
-            <br />
-            <button class='signupbtn' type='submit'>
-              Sign Up
-            </button>
+            <div id='signup-part'>
+              <h2>Sign Up</h2>
+              <form class='signup'>
+                <label for='firstName'>First Name</label>
+                <input
+                  type='text'
+                  placeholder='Enter First Name'
+                  name='firstName'
+                />
+                <br />
+                <label for='lastName'>Last Name</label>
+                <input
+                  type='text'
+                  placeholder='Enter Last Name'
+                  name='lastName'
+                />
+                <br />
+                <label for='email'>Email Address</label>
+                {/* <br /> */}
+                <input
+                  type='email'
+                  placeholder='Enter Email'
+                  name='email'
+                  required
+                />
+                <br />
+                <label for='password'>Password</label>
+                {/* <br /> */}
+                <input
+                  type='password'
+                  placeholder='Enter New Password'
+                  id='student-password'
+                  name='password'
+                  required
+                />
+                <br />
+                <input type='submit' value='Sign Up' />
+              </form>
+            </div>
           </div>
         </div>
       </div>
