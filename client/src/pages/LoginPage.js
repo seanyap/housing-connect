fca9688cadebe3a37a2c59e854940a4a958b95f5
Sean Yap <syap0317@gmail.com>
2021-12-06 16:40:46 -0500

    change stylerule from h2 to more specific

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,89 +1,89 @@
-import React from 'react';
+import React from "react";
 // import NavBar from '../components/NavBar';
-import '../styles/login.css';
+import "../styles/login.css";
 
 class LoginPage extends React.Component {
   render() {
     return (
       <div>
-        <div class='body'>
-          <div class='login-body'>
-            <div id='login'>
-              <h2>Sign In</h2>
-              <form class='login'>
-                <label for='username'>Email Address</label>
-                <input type='text' placeholder='Enter Email' name='username' />
+        <div class="body">
+          <div class="login-body">
+            <div id="login">
+              <h2 id="login-signin-heading">Sign In</h2>
+              <form class="login">
+                <label for="username">Email Address</label>
+                <input type="text" placeholder="Enter Email" name="username" />
                 <br />
-                <label for='password'>Password</label>
+                <label for="password">Password</label>
                 <input
-                  type='password'
-                  placeholder='Enter Password'
-                  name='password'
+                  type="password"
+                  placeholder="Enter Password"
+                  name="password"
                 />
                 <br />
                 <p>Don't have an account? Sign up for one for free!</p>
-                <input type='submit' value='Sign In' />
+                <input type="submit" value="Sign In" />
               </form>
             </div>
-            <div id='signup-part'>
-              <h2>Sign Up</h2>
-              <form class='signup'>
-                <div class='checkboxes'>
-                  <div class='check'>
+            <div id="signup-part">
+              <h2 id="login-signup-heading">Sign Up</h2>
+              <form class="signup">
+                <div class="checkboxes">
+                  <div class="check">
                     <input
-                      type='checkbox'
-                      id='1'
-                      name='box'
-                      class='boxes'
-                      onclick='onlyOne(this.id)'
+                      type="checkbox"
+                      id="1"
+                      name="box"
+                      class="boxes"
+                      onclick="onlyOne(this.id)"
                     />
-                    <label for='student-box'>Homeowner</label>
+                    <label for="student-box">Homeowner</label>
                   </div>
                   {/* <br /> */}
-                  <div class='check'>
+                  <div class="check">
                     <input
-                      type='checkbox'
-                      id='2'
-                      class='boxes'
-                      onclick='onlyOne(this.id)'
+                      type="checkbox"
+                      id="2"
+                      class="boxes"
+                      onclick="onlyOne(this.id)"
                     />
-                    <label for='instructor-box'>Tenant</label>
+                    <label for="instructor-box">Tenant</label>
                   </div>
                 </div>
-                <label for='firstName'>First Name</label>
+                <label for="firstName">First Name</label>
                 <input
-                  type='text'
-                  placeholder='Enter First Name'
-                  name='firstName'
+                  type="text"
+                  placeholder="Enter First Name"
+                  name="firstName"
                 />
                 <br />
-                <label for='lastName'>Last Name</label>
+                <label for="lastName">Last Name</label>
                 <input
-                  type='text'
-                  placeholder='Enter Last Name'
-                  name='lastName'
+                  type="text"
+                  placeholder="Enter Last Name"
+                  name="lastName"
                 />
                 <br />
-                <label for='email'>Email Address</label>
+                <label for="email">Email Address</label>
                 {/* <br /> */}
                 <input
-                  type='email'
-                  placeholder='Enter Email'
-                  name='email'
+                  type="email"
+                  placeholder="Enter Email"
+                  name="email"
                   required
                 />
                 <br />
-                <label for='password'>Password</label>
+                <label for="password">Password</label>
                 {/* <br /> */}
                 <input
-                  type='password'
-                  placeholder='Enter New Password'
-                  id='student-password'
-                  name='password'
+                  type="password"
+                  placeholder="Enter New Password"
+                  id="student-password"
+                  name="password"
                   required
                 />
                 <br />
-                <input type='submit' value='Sign Up' />
+                <input type="submit" value="Sign Up" />
               </form>
             </div>
           </div>
