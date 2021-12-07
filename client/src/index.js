c11313acb7e34af64aa0ad6900eab49df256d5bf
Sean Yap <syap0317@gmail.com>
2021-12-06 23:08:12 -0500

    removed nav bar component

--- client/src/index.js
+++ client/src/index.js
@@ -1,37 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import * as serviceWorker from './serviceWorker';
-import { Link, Switch, Route, BrowserRouter } from 'react-router-dom';
-import NavBar from './components/NavBar';
-import App from './pages/App';
-import ResultPage from './pages/ResultPage';
-import DashboardPage from './pages/DashboardPage';
-import LoginPage from './pages/LoginPage';
-import NewListingPage from './pages/NewListingPage';
-import ListingPage from './pages/ListingPage';
-import TenantInfoPage from './pages/TenantInfoPage';
+import * as serviceWorker from "./serviceWorker";
+import { Link, Switch, Route, BrowserRouter } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import App from "./pages/App";
+import ResultPage from "./pages/ResultPage";
+import DashboardPage from "./pages/DashboardPage";
+import LoginPage from "./pages/LoginPage";
+import NewListingPage from "./pages/NewListingPage";
+import ListingPage from "./pages/ListingPage";
+import TenantInfoPage from "./pages/TenantInfoPage";
 
 const routing = (
   <>
     <BrowserRouter>
-      <Link to='/'>
-        <NavBar isLoggedIn={false} />
-      </Link>
       <Switch>
-        <Route exact path='/' component={App} />
-        <Route path='/houses' component={ResultPage} />
-        <Route path='/dashboard' component={DashboardPage} />
-        <Route path='/login' component={LoginPage} />
-        <Route path='/tenant-info' component={TenantInfoPage} />
-        <Route path='/new-listing' component={NewListingPage} />
-        <Route path='/list/:id' component={ListingPage} />
+        <Route exact path="/" component={App} />
+        <Route path="/houses" component={ResultPage} />
+        <Route path="/dashboard" component={DashboardPage} />
+        <Route path="/login" component={LoginPage} />
+        <Route path="/tenant-info" component={TenantInfoPage} />
+        <Route path="/new-listing" component={NewListingPage} />
+        <Route path="/list/:id" component={ListingPage} />
       </Switch>
     </BrowserRouter>
   </>
 );
 
-ReactDOM.render(routing, document.getElementById('root'));
+ReactDOM.render(routing, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
