481f0d2626ea9f68acd9faa3ad576d9fe8f9a7d3
Sean Yap <syap0317@gmail.com>
2021-12-06 20:30:58 -0500

    areplaced static data with data  returned from api

--- client/src/pages/ResultPage.js
+++ client/src/pages/ResultPage.js
@@ -8,24 +8,50 @@ export default class ResultPage extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      rent: 0,
+      address: "",
       listItems: [],
     };
   }
 
   componentDidMount() {
     // TODO: add logic to fetch for list items & instantiate the ListItem component
+    fetch("/api/listings")
+      .then((res) => res.json())
+      .then((data) =>
+        this.setState({ rent: data.rent, address: data.address })
+      );
   }
 
   render() {
     return (
       <div id="result-container">
+        {/* TODO: disable map rendering when in mobile device*/}
         <SideMap></SideMap>
         {/* TODO: run js loop to instantiate all list item in our state*/}
         {/* 3 dummy placeholder */}
         <div id="posts">
-          <ListItem id="post" name="1"></ListItem>
-          <ListItem id="post" name="2"></ListItem>
-          <ListItem id="post" name="3"></ListItem>
+          <ListItem
+            id="post"
+            name="1"
+            rent={this.state.rent}
+            address={this.state.address}
+            img="../../images/list-items-examples/apt-1.jpeg"
+          ></ListItem>
+          <ListItem
+            id="post"
+            name="2"
+            rent={this.state.rent}
+            address={this.state.address}
+            img="../../images/list-items-examples/apt-2.jpeg"
+          ></ListItem>
+          <ListItem
+            id="post"
+            name="3"
+            rent={this.state.rent}
+            address={this.state.address}
+            img="../../images/list-items-examples/apt-3.jpeg"
+          ></ListItem>
         </div>
       </div>
     );
